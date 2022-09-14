de12732e9d465612da3e2e968e1d41204d62cf7f
Joao M <jmatheus_05@hotmail.com>
2022-09-14 17:55:49 -0300

    componentizando botão e iniciando tela de cadastro

---
+++ MyHealth/src/components/CreateAccount.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+const CreateAccount = () => (
+    <View style={styles.container}>
+
+    </View>
+);
+
+const styles = StyleSheet.create({
+
+});
+
+export default CreateAccount;
--- MyHealth/src/components/Initial.js
+++ MyHealth/src/components/Initial.js
@@ -3,30 +3,19 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import InitialTitle from "./initial-components/InitialTitle"
 import InitialSubtitle from "./initial-components/InitialSubtitle"
 import InitialLogin from "./initial-components/InitialLogin";
+import InitialButton from "./initial-components/InitialButton";
 
 const Initial = () => (
     <View style={styles.container}>
         <InitialTitle value="MyHealth" />
         <InitialSubtitle value="Controle as suas vacinas e fique seguro" />
-        <View style={{ marginTop:50 }}>
+        <View style={{ marginTop: 50 }}>
             <InitialLogin label="E-mail" />
             <InitialLogin label="Senha" />
         </View>
-        <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.buttonTouchableOpacity.button1}>
-                <Text style={styles.buttonText}>Entrar</Text>
-            </TouchableOpacity>
-        </View>
-        <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.buttonTouchableOpacity.button2}>
-                <Text style={styles.buttonText}>Criar minha conta</Text>
-            </TouchableOpacity>
-        </View>
-        <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.buttonTouchableOpacity.button3}>
-                <Text style={styles.buttonText}>Esqueci minha senha</Text>
-            </TouchableOpacity>
-        </View>
+        <InitialButton value="Entrar" backgroundColor="#49b976" width={120} />
+        <InitialButton value="Criar minha conta" backgroundColor="#419ed7" width={200} />
+        <InitialButton value="Esqueci minha senha" backgroundColor="#b5c7d1" width={220} />
     </View>
 );
 
@@ -36,32 +25,6 @@ const styles = StyleSheet.create({
         paddingTop: 50,
         backgroundColor: "#d3dcdb",
     },
-    buttonContainer: {
-        marginTop: 40,
-        alignItems: "center",
-    },
-    buttonTouchableOpacity: {
-        button1: {
-            elevation: 8,
-            backgroundColor:"#49b976",
-            width: 120,
-        },
-        button2: {
-            elevation: 8,
-            backgroundColor:"#419ed7",
-            width: 200,
-        },
-        button3: {
-            elevation: 8,
-            backgroundColor:"#b5c7d1",
-            width: 220,
-        },
-    },
-    buttonText: {
-        textAlign: "center",
-        fontSize: 20,
-        paddingVertical: 6,
-    }
 });
 
 export default Initial;
